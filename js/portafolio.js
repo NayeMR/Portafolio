357d144b4e07bd4f5a4c0549e1b13b589e174355
NayeMR <naye.mirandys@gmail.com>
2016-11-22 14:01:07 -0900

    version mas

--- js/portafolio.js
+++ js/portafolio.js
@@ -1,13 +1,21 @@
-alert("hola");
-$(document).ready(
-	
-	$("#responsive_headline").fitText();
-	//fullpage
-	$('#fullpage').fullpage({
-		anchors: ['firstPage', 'secondPage', '3rdPage'],
-		sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-		navigation: true,
-		navigationPosition: 'right',
-		navigationTooltips: ['First page', 'Second page', 'Third and last page']
-	});
-);
+$("#responsive_headline").fitText();
+//fullpage
+/*$('#fullpage').fullpage({
+	anchors: ['firstPage', 'secondPage', '3rdPage'],
+	sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+	navigation: true,
+	navigationPosition: 'right',
+	navigationTooltips: ['First page', 'Second page', 'Third and last page']
+});*/
+//paralax
+$('section[data-type="parallax_section"]').each(function(){
+    var $bgobj = $(this); // Variable para asignacion de objeto
+    $(window).scroll(function() {
+    	$window = $(window);
+        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+        // cordinadas del background
+        var coords = '50% '+ yPos + 'px';
+        // moviendo el background
+        $bgobj.css({ backgroundPosition: coords });
+    });
+});
